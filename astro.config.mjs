93e04d0be120df10b56e6634ed94581478648ef2
David Demaree <david@demaree.me>
2026-08-11 17:13:33 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,63 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import markdoc from '@astrojs/markdoc';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [markdoc()],
+  adapter: vercel(),
+
+
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "die-grotesk-vf",
+    cssVariable: "--font-die-grotesk-vf",
+    options: {
+      variants: [
+        {
+          src: ['./src/assets/fonts/die-grotesk-vf-roman.woff2'],
+          weight: '100 900',
+          style: 'normal'
+        },
+        {
+          src: ['./src/assets/fonts/die-grotesk-vf-italic.woff2'],
+          weight: '100 900',
+          style: 'italic'
+        }
+      ]
+    }
+  }, {
+    provider: fontProviders.local(),
+    name: "gorton-perfected-vf",
+    cssVariable: "--font-gorton-perfected-vf",
+    options: {
+      variants: [
+        {
+          src: ['./src/assets/fonts/GortonPerfectedVF.woff2'],
+          weight: '100 900',
+          style: 'normal'
+        }
+      ]
+    }
+  }, {
+    provider: fontProviders.local(),
+    name: "berkeley-mono-vf",
+    cssVariable: "--font-berkeley-mono-vf",
+    options: {
+      variants: [
+        {
+          src: ['./src/assets/fonts/Berkeley Mono Variable.woff2'],
+          weight: '100 900',
+          style: 'normal'
+        }
+      ]
+    }
+  }]
+});
